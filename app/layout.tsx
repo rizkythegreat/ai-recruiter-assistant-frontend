df85b44157ff15adeb2aba183c9434add886779a
Rizky Rahman Salam <clashikik@gmail.com>
2026-03-16 18:47:32 +0700

    feat: add og image preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,69 +1,65 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Sidebar } from "@/components/sidebar";
-import AppsProvider from "./context/AppsContext";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Sidebar } from '@/components/sidebar';
+import AppsProvider from './context/AppsContext';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
-  title: "RecruitAI - AI-Powered Recruitment Assistant",
-  description: "Efficiently rank and analyze candidates with AI.",
+  title: 'RecruitAI - AI-Powered Recruitment Assistant',
+  description: 'Efficiently rank and analyze candidates with AI.',
   icons: {
-    icon: "/logo-cv-assistants.png"
+    icon: '/logo-cv-assistants.png'
   },
   metadataBase: new URL('https://ai-recruiter.rizkyrahmansalam.my.id/'),
   openGraph: {
-    title: "RecruitAI - AI-Powered Recruitment Assistant",
-    description: "Efficiently rank and analyze candidates with AI.",
-    url: "https://ai-recruiter.rizkyrahmansalam.my.id/",
+    title: 'RecruitAI - AI-Powered Recruitment Assistant',
+    description: 'Efficiently rank and analyze candidates with AI.',
+    url: 'https://ai-recruiter.rizkyrahmansalam.my.id/',
     siteName: 'RecruitAI',
     images: [
       {
-        url: '/screenshots/preview-1.png',
+        url: '/screenshots/og-preview.png',
         width: 1200,
         height: 630,
-        alt: "RecruitAI Dashboard Preview"
+        alt: 'RecruitAI Dashboard Preview'
       }
     ],
     locale: 'id_ID',
     type: 'website'
   },
   twitter: {
-    card: "summary_large_image",
-    title: "RecruitAI - AI-Powered Recruitment Assistant",
-    description: "Efficiently rank and analyze candidates with AI.",
-    images: ["/screenshots/preview-1.png"],
-  },
+    card: 'summary_large_image',
+    title: 'RecruitAI - AI-Powered Recruitment Assistant',
+    description: 'Efficiently rank and analyze candidates with AI.',
+    images: ['/screenshots/preview-1.png']
+  }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" data-theme="cupcake">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AppsProvider>
           <div className="flex flex-col lg:flex-row h-screen overflow-hidden">
-          <Sidebar />
-          <main className="flex-1 overflow-y-auto p-6 lg:p-12 bg-base-100">
-            <div className="max-w-6xl mx-auto w-full">
-              {children}
-            </div>
-          </main>
-        </div>
+            <Sidebar />
+            <main className="flex-1 overflow-y-auto p-6 lg:p-12 bg-base-100">
+              <div className="max-w-6xl mx-auto w-full">{children}</div>
+            </main>
+          </div>
         </AppsProvider>
       </body>
     </html>
